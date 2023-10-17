0b7359a79cd30487108fa9f494680a1ea641665c
SlimaneAzem <104330100+SlimaneAzem@users.noreply.github.com>
2023-10-18 01:33:34 +0200

    Fonctions de base (partie 1)

--- Projet/base.ml
+++ Projet/base.ml
@@ -33,3 +33,36 @@ let echange grille v1 v2 =
 (* On declare un type direction *)
 type direction = Droite | Bas | Gauche | Haut;;
 
+(* Check si un deplacement est possible *)
+let deplacement_possible position tailleGrille direction = 
+  match direction with
+  | Haut -> (position >= tailleGrille)
+  | Bas -> (position < tailleGrille*(tailleGrille-1))
+  | Gauche -> (position mod tailleGrille <> 0)
+  | Droite -> (position mod tailleGrille <> (tailleGrille-1));;
+
+(* Donne la nouvelle position du 0 selon une direction (on suppose le deplacement possible) *)
+let nouvelle_position tailleGrille oldPos direction = 
+  match direction with
+  | Haut -> (oldPos - tailleGrille)
+  | Bas -> (oldPos + tailleGrille)
+  | Gauche -> (oldPos - 1)
+  | Droite -> (oldPos + 1);;
+
+let deplacer puzzle direction = 
+  let (grille, tailleGrille) = puzzle
+  in let position_0 = (position grille 0)
+      in if (deplacement_possible position_0 tailleGrille direction) then let valeur_nouvelle_case = (valeur grille (nouvelle_position tailleGrille position_0 direction))
+                                                                          in (echange grille 0 valeur_nouvelle_case)
+                                                                    else grille;;
+      
+let test_unaire_deplacer resultat test = 
+  if resultat=test then "good Shit"
+                    else "No good shit";;
+
+(* test_unaire_deplacer [1; 2; 3; 4; 5; 0; 6; 7; 8] (deplacer ([1;2;3;4;0;5;6;7;8],3) Droite);;
+test_unaire_deplacer [1; 2; 3; 4; 7; 5; 6; 0; 8] (deplacer ([1;2;3;4;0;5;6;7;8],3) Bas);;
+test_unaire_deplacer [1; 2; 3; 0; 4; 5; 6; 7; 8] (deplacer ([1;2;3;4;0;5;6;7;8],3) Gauche);;
+test_unaire_deplacer [1; 0; 3; 4; 2; 5; 6; 7; 8] (deplacer ([1;2;3;4;0;5;6;7;8],3) Haut);;
+
+test_unaire_deplacer [0; 1; 2; 3; 4; 5; 6; 7; 8] (deplacer ([0;1;2;3;4;5;6;7;8],3) Gauche);; *)
